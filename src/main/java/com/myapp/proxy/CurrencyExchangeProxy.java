7ca1fcd6274c488c393fd26a6d7025f7decd9c5a
Subhashis <subhashisz@gmail.com>
2021-10-23 21:16:30 +0530

    first commit

---
+++ src/main/java/com/myapp/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,16 @@
+package com.myapp.proxy;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.myapp.bean.CurrencyConversion;
+
+//@FeignClient(name="currency-exchange", url="localhost:8000")
+@FeignClient(name = "currency-exchange")
+public interface CurrencyExchangeProxy {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public CurrencyConversion retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
+
+}
